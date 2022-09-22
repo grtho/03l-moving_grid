368b9de029595f1b41b284133b24d242872364ca
Griffin Thompson <47836676+grtho@users.noreply.github.com>
2022-09-22 10:16:37 -0600

    Update main.hpp to reflect its actual contents, instead of it being an empty file.

--- main.hpp
+++ main.hpp
@@ -5,4 +5,8 @@
 #ifndef INC_03L_MOVING_GRID_MAIN_HPP
 #define INC_03L_MOVING_GRID_MAIN_HPP
 
+struct Point{int x, y;};
+enum Direction {NORTH = 1, EAST, SOUTH, WEST};
+void move(Point&, Direction);
+
 #endif //INC_03L_MOVING_GRID_MAIN_HPP
